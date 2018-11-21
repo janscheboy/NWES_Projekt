ac074ca97c1804df233d7b488b17ac577548b66b
janscheboy <44437857+janscheboy@users.noreply.github.com>
2018-11-21 15:22:06 +0100

    Saving my code just in case

--- MegaProjekt/c1201.c
+++ MegaProjekt/c1201.c
@@ -1,9 +1,16 @@
 #include <avr/io.h>
 #include "c1201.h"
-#include "hc595.h"
-#include "board.h"
+//#include "hc595.h"
+//#include "board.h"
+#include "spi.h"
 
 void c1201_init() {
+	spi_init();
+}
 
+void c1201_transmit(uint8_t dis1, uint8_t dis2) {
+	spi_select_slave();
+	spi_transfer(dis1);
+	spi_transfer(dis2);
 }
 
--- MegaProjekt/main.c
+++ MegaProjekt/main.c
@@ -1,10 +1,5 @@
-/*
- * MegaProjekt.c
- *
- * Created: 10.10.2018 15:07:58
- */ 
-
 #include <avr/io.h>
+#include <util/delay.h>
 #include "c1201.h"
 #include "hc595.h"
 #include "pwm.h"
@@ -14,9 +9,28 @@
 
 int main(void)
 {
+	BUTTON_LOW_PORT.PIN0CTRL = PORT_OPC_PULLUP_gc;
+	BUTTON_LOW_PORT.PIN1CTRL = PORT_OPC_PULLUP_gc;
+	BUTTON_LOW_PORT.PIN2CTRL = PORT_OPC_PULLUP_gc;
+	BUTTON_LOW_PORT.PIN3CTRL = PORT_OPC_PULLUP_gc;
+	
 	c1201_init();
     while (1)
     {
+		c1201_transmit(0x03, 0x03);
+		_delay_ms(100);
+		if (!(BUTTON_LOW_PORT.IN & BUTTON0_PIN_bm)) {
+			TCE0.CCA = TCE0.PER;     // Duty cycle 100%: LED not dimmed
+		}
+		if (!(BUTTON_LOW_PORT.IN & BUTTON1_PIN_bm)) {
+			TCE0.CCA = TCE0.PER/2;   // Duty cycle 50%: LED dimmed to 50%
+		}
+		if (!(BUTTON_LOW_PORT.IN & BUTTON2_PIN_bm)) {
+			TCE0.CCA = TCE0.PER/4;   // Duty cycle 25%: LED dimmed to 25%
+		}
+		if (!(BUTTON_LOW_PORT.IN & BUTTON3_PIN_bm)) {
+			TCE0.CCA = 0x0000;       // Duty cycle 0%: LED switched off
+		}
     }
 }
 
